f9fd9b3763f56371d742fde215b8cc93f5e7f78b
Henrique elias <hcbe2004@gmail.com>
2018-11-13 12:51:18 -0200

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
     var index = 0;
     var content = document.getElementById("content");
     var config = {
-        timeout: 300000,
+        timeout: 30000,
         url: 'https://app.vivifyscrum.com/boards/',
         boards: 
             [
